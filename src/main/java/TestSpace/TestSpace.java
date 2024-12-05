6d32e1f0aea95a8daf9a37b3c02ac20a90995b28
iloveMlord <ilovemilord.op@gmail.com>
2024-12-05 14:27:24 +0700

    Reconstruct login signup

--- src/main/java/TestSpace/TestSpace.java
+++ src/main/java/TestSpace/TestSpace.java
@@ -7,6 +7,7 @@ import com.google.gson.GsonBuilder;
  * @author huynhkimai
  * @author ilovemilord
  */
+@SuppressWarnings("unused")
 public class TestSpace {
     public static void main(String[] args) {
 
---
+++ src/main/java/UIMainStream/MainEntry.java
@@ -0,0 +1,7 @@
+package UIMainStream;
+
+public class MainEntry {
+    public static void main(String[] args) {
+
+    }
+}
