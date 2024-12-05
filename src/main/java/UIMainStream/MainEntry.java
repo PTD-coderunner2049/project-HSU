1c7b520ecd8df478990da9e54a21f8cf7a3228d8
iloveMlord <ilovemilord.op@gmail.com>
2024-12-05 14:59:07 +0700

    Refurbish Login & SignUp fram, ADD MainEntry, get ready for more backend.

--- src/main/java/UIMainStream/MainEntry.java
+++ src/main/java/UIMainStream/MainEntry.java
@@ -2,6 +2,12 @@ package UIMainStream;
 
 public class MainEntry {
     public static void main(String[] args) {
+        Login logInFrame = new Login();
+        SignUp signUpFrame = new SignUp();
 
+        logInFrame.bond(signUpFrame);
+        signUpFrame.bond(logInFrame);
+
+        logInFrame.setVisible(true);
     }
 }
